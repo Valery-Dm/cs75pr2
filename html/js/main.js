e3be5f7ef5c2d0274d235779ef471355bafec171
Valery-Dm <dmitriev.valery@gmail.com>
2015-03-22 21:16:16 +0300

    load gmap

--- html/js/main.js
+++ html/js/main.js
@@ -1,55 +1,67 @@
+/**
+ * All functionality is here.
+ */
+var map = "",
+	marker = "",
+	initialize,
+	addMarker,
+	addPolyline,
+	addInfoWindow;
 
-var main = function () {
-	var map = "";
-	var marker = "";
+initialize = function () {
+	"use strict";
+	var mapOptions = {
+		center: new google.maps.LatLng(37.775362, -122.417564),
+		zoom: 12,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	//console.log(map);
+};
 
-	  function initialize() {
+addMarker = function () {
+	"use strict";
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(37.775362, -122.417564),
+		title: "I am a marker!"
+	});
+	marker.setMap(map);
+};
 
-		var mapOptions = {
-		  center: new google.maps.LatLng(37.775362, -122.417564),
-		  zoom: 12,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-			map = new google.maps.Map(document.getElementById("map_canvas"),
-			mapOptions);
-	  }
-
-	  function addMarker() {
-			  marker = new google.maps.Marker({
-			  position: new google.maps.LatLng(37.775362, -122.417564),
-			  title: "I am a marker!"
-		  });
-
-		  marker.setMap(map);
-	  }
-
-	  function addPolyline() {
-		  var polylineCoordinates = [
+addPolyline = function () {
+	"use strict";
+	var polylineCoordinates = [
 			new google.maps.LatLng(37.775362, -122.417564),
 			new google.maps.LatLng(37.7849, -122.4522),
-			new google.maps.LatLng(37.7805, -122.4725),
-			];
+			new google.maps.LatLng(37.7805, -122.4725)
+		],
 
-		  var polylinePath = new google.maps.Polyline({
+		polylinePath = new google.maps.Polyline({
 			path: polylineCoordinates,
 			strokeColor: "#FF0000",
 			strokeOpacity: 1.0,
 			strokeWeight: 2
-		}); 
+		});
 
-		polylinePath.setMap(map);
-	}
+	polylinePath.setMap(map);
+};
 
-	function addInfoWindow() {
-		var infowindow = new google.maps.InfoWindow({
+addInfoWindow = function () {
+	"use strict";
+	var infowindow = new google.maps.InfoWindow({
 		content: "<h1 style='color:blue'>I am an info window!</h1>",
-		position: new google.maps.LatLng(37.7805, -122.4725), 
-		});
+		position: new google.maps.LatLng(37.7805, -122.4725)
+	});
 
-		google.maps.event.addListener(marker, 'click', function() {
-		infowindow.open(map,marker);
-		});
-	}
-}
+	google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, marker);
+	});
+};
 
-window.onload = main;
+window.onload = function () {
+	"use strict";
+	initialize();
+	addPolyline();
+	addMarker();
+	addInfoWindow();
+};
